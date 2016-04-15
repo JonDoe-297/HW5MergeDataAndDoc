9f49fdee60190089b48f95dcb623de3800b0d0cb
JonDoe297 <ywx199503@126.com>
2016-04-15 17:57:31 +0800

    third

--- sy2/Test.cs
+++ sy2/Test.cs
@@ -21,5 +21,14 @@ namespace sy2
             Assert.That(output.ToString(), Is.EqualTo("Tom先生(身份证字号c1234234)为本校专任教师，聘期2年。\r\n\r\n\r\n"));
 
         }
+        public void Test2()
+        {
+            StringReader input = new StringReader("Jolin	a2232343	4年");
+            StringReader input1 = new StringReader("${中文姓名}先生(身份证字号${身份证字号})为本校专任教师，聘期${年数}。");
+            StringWriter output = new StringWriter();
+            MergeDataAndDoc.Program.StringMethod(input, input1, output);
+            Assert.That(output.ToString(), Is.EqualTo("Jolin先生(身份证字号a2232343)为本校专任教师，聘期4年。\r\n\r\n\r\n"));
+
+        }
     }
 }
